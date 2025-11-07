e9ebb90b37bfd70ee96e96b210b7a379de597164
BRobinson <b.robinson.cse@gmail.com>
2025-11-06 17:41:22 -0800

    [Feature] Adding UI for manual card entry; Addig logic for scene switching

--- src/main/java/flashcards/SceneManager.java
+++ src/main/java/flashcards/SceneManager.java
@@ -1,14 +1,54 @@
 package flashcards;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 /** This class defines the many functions needed to manage the frontend */
 public class SceneManager {
+    // TODO: attach this to a button on main scene
+
+    /**
+     * This method declares what it means to create a new stage 
+     *   that runs in addition to the stages already running
+     *   @param - the path to the .fxml file to load
+     */
+    private void NewStage(String path) 
+    {
+        Stage newStage = new Stage();
+        Parent root;
+
+        try {
+            root = FXMLLoader.load(getClass().getResource(path));
+            Scene scene = new Scene(root);
+    
+            newStage.setTitle("Flashcards");
+            newStage.setScene(scene);
+            newStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Creates a new data entry window
+     */
+    public void newDataEntry()
+    {
+        NewStage("/CardEntry.fxml");
+    }
+
+    
+
+    // Data Manipulation - button functionality
     @FXML
     private Label CardText;
 
     private int index;
+
     /** 
      * Flips Card to the other Side
      *    - if already showing Front: flip to back
